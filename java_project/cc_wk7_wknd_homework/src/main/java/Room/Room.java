9479037f6bb9796c00197762702465475f6773b7
Mike-Heneghan <m.heneghan@live.co.uk>
2018-04-29 16:49:08 +0100

    Healer heals the hero with lowest health and returns the correct string.

--- java_project/cc_wk7_wknd_homework/src/main/java/Room/Room.java
+++ java_project/cc_wk7_wknd_homework/src/main/java/Room/Room.java
@@ -47,65 +47,109 @@ public class Room {
         heroes = heroesFromHallway;
     }
 
-    //For encounter- collectTreasure to be used as a conditional if any heroes remain alive.
-    public void heroesCollectTreasure(){
-        Hero hero = heroes.get(0);
-        hero.addToInventory(this.treasure);
-        this.treasure = null;
+
+    public boolean isFightOver(){
+        if (monster.getHp()<= 0 || areAllHeroesDefeated()) {
+            return true;
+        }
+        return false;
     }
 
-    //For encounter - returning our heroes at the end of a fight if any remain alive afterwards.
-    public ArrayList<Hero> returnRemainingHeroes(){
-        ArrayList<Hero> remainingHeroes = new ArrayList<>();
-        for (Hero hero : heroes){
-            if(!hero.characterDead()){
-                remainingHeroes.add(hero);
+    public boolean areAllHeroesDefeated() {
+        int aliveCounter = 0;
+        for (Hero h : heroes) {
+            if (h.getHp() > 0) {aliveCounter++;}
+        }
+        if (aliveCounter == 0) { return true; }
+
+        else return false;
+    }
+
+    public void heroStandardMove(Hero hero){
+        if (hero.getHp() > 0){
+            if (hero.getType() == "Healer"){
+                Narrator.getInstance().addStoryLine(hero.standardMove(heroWithLowestHealth()));
+            }
+            else Narrator.getInstance().addStoryLine(hero.standardMove(monster));}
+        else Narrator.getInstance().addStoryLine(hero.getName() + " is dead!");
+
+    }
+
+    public void heroSignatureMove(Hero hero){
+        if (hero.getHp() > 0){
+            if (hero.getType() == "Healer"){
+                hero.signatureMove(heroWithLowestHealth());
+            }
+            hero.signatureMove(monster);}
+        else Narrator.getInstance().addStoryLine(hero.getName() + " is dead!");
+        }
+
+    public Hero heroWithLowestHealth(){
+        Hero heroToHeal = null;
+        for (Hero h: heroes) {
+            if (heroToHeal == null){
+                heroToHeal = h;
+            }
+            else {
+                if (heroToHeal.getHp() > h.getHp()){
+                    heroToHeal = h;
+                }
             }
         }
-        for (Hero h: remainingHeroes) {
-            Narrator.getInstance().addStoryLine(h.getName() + " survived the encounter" + " Treasure: " + h.getTotalTreasureValue() + " Health Points: " + h.getHp());
-        }
-        return remainingHeroes;
+        return heroToHeal;
     }
 
-    //For encounter. The heroes will all have a go at monster and monster will have a go at heroes.
-    public void heroesFightMonster(){
-        for(Hero hero : heroes){
-            if (hero.getType() == "Healer"){
-                Narrator.getInstance().addStoryLine(hero.standardMove(heroes.get(0)));
-                Narrator.getInstance().addStoryLine(monster.standardMove(hero)); }
-            else {Narrator.getInstance().addStoryLine(hero.standardMove(monster));
-            Narrator.getInstance().addStoryLine(monster.standardMove(hero));}
-        }
-
-    }
-
-    //For encounter. A boolean to check is there are any heroes left alive after the fight round. Can probably remove this and use remainingHeroes.isempty() as a conditional.
-    public boolean allHeroesDead(){
-        return returnRemainingHeroes().isEmpty();
-    }
-
-    //For encounter. A loop to have heroes and creatures fight until there is a winner.
-    public void fight(){
-        while(!monster.characterDead() && !allHeroesDead()){
-            heroesFightMonster();
-        }
-
-    }
-
-    public void encounter(){
-        fight();
-        if(!returnRemainingHeroes().isEmpty()){
-            heroesCollectTreasure();
-
-        }
-    }
-
-
-
-
-
-//    public ArrayList<Character> encounter( ArrayList<Character> heroes){
+//    //For encounter- collectTreasure to be used as a conditional if any heroes remain alive.
+//    public void heroesCollectTreasure(){
+//        Hero hero = heroes.get(0);
+//        hero.addToInventory(this.treasure);
+//        this.treasure = null;
+//    }
 //
+//    //For encounter - returning our heroes at the end of a fight if any remain alive afterwards.
+//    public ArrayList<Hero> returnRemainingHeroes(){
+//        ArrayList<Hero> remainingHeroes = new ArrayList<>();
+//        for (Hero hero : heroes){
+//            if(!hero.characterDead()){
+//                remainingHeroes.add(hero);
+//            }
+//        }
+//        for (Hero h: remainingHeroes) {
+//            Narrator.getInstance().addStoryLine(h.getName() + " survived the encounter" + " Treasure: " + h.getTotalTreasureValue() + " Health Points: " + h.getHp());
+//        }
+//        return remainingHeroes;
+//    }
+//
+//    //For encounter. The heroes will all have a go at monster and monster will have a go at heroes.
+//    public void heroesFightMonster(){
+//        for(Hero hero : heroes){
+//            if (hero.getType() == "Healer"){
+//                Narrator.getInstance().addStoryLine(hero.standardMove(heroes.get(0)));
+//                Narrator.getInstance().addStoryLine(monster.standardMove(hero)); }
+//            else {Narrator.getInstance().addStoryLine(hero.standardMove(monster));
+//            Narrator.getInstance().addStoryLine(monster.standardMove(hero));}
+//        }
+//
+//    }
+//
+//    //For encounter. A boolean to check is there are any heroes left alive after the fight round. Can probably remove this and use remainingHeroes.isempty() as a conditional.
+//    public boolean allHeroesDead(){
+//        return returnRemainingHeroes().isEmpty();
+//    }
+//
+//    //For encounter. A loop to have heroes and creatures fight until there is a winner.
+//    public void fight(){
+//        while(!monster.characterDead() && !allHeroesDead()){
+//            heroesFightMonster();
+//        }
+//
+//    }
+//
+//    public void encounter(){
+//        fight();
+//        if(!returnRemainingHeroes().isEmpty()){
+//            heroesCollectTreasure();
+//
+//        }
 //    }
 }
