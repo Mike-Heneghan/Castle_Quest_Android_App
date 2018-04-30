117d10a094e37815a3dc300ec121d239c2d5f698
Mike-Heneghan <m.heneghan@live.co.uk>
2018-04-30 23:20:28 +0100

    Hero option arrayList created with descriptions of heroes.

--- android_project/app/src/main/java/com/example/mike/fantasygame/DragonCastle/Activities/CharacterSelectionActivity.java
+++ android_project/app/src/main/java/com/example/mike/fantasygame/DragonCastle/Activities/CharacterSelectionActivity.java
@@ -3,6 +3,8 @@ package com.example.mike.fantasygame.DragonCastle.Activities;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.mike.fantasygame.DragonCastle.DataPeristence.ApplicationState;
+import com.example.mike.fantasygame.DragonCastle.DataPeristence.SharedPreferenceHelper;
 import com.example.mike.fantasygame.R;
 
 public class CharacterSelectionActivity extends AppCompatActivity {
@@ -11,5 +13,7 @@ public class CharacterSelectionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_character_selection);
+
+
     }
 }
---
+++ android_project/app/src/main/java/com/example/mike/fantasygame/DragonCastle/Characters/HeroOptionsData.java
@@ -0,0 +1,17 @@
+package com.example.mike.fantasygame.DragonCastle.Characters;
+
+import java.util.ArrayList;
+
+public class HeroOptionsData {
+
+    ArrayList<HeroOption> list;
+
+    public HeroOptionsData(){
+        list = new ArrayList<>();
+        list.add(0, new HeroOption(1, "Knight", "The Knight wields a strong shield which reduces the damage suffered by 50%.", "HP: 100", "The Knight attacks accurately with a sword.", "At close counters the Knight can't miss stabbing an opponent with a concealed dagger."));
+        list.add(1, new HeroOption(2, "Dwarf", "The Dwarf dons armour shield which reduces the damage they suffer by 25%.", "HP: 100", "The Dwarf swings a mighty axe which inflicts high damage.", "Dwarves always carry 5 Throwing axes, use them wisely."));
+       list.add(2, new HeroOption(3, "Barbarian", "All barbarians wear arm guards to reduce damage they receive by 10% without slowing them down.", "HP: 100", "The Barbarian wields a powerful but accurate club.", "When enraged the Barbarian can go into a fury that allows them to feel no pain and kill their opponent at the cost of their own life especially when health is low."));
+        list.add(3, new HeroOption(4, "Warlock", "The Warlock is trained in magic and can cast powerful spells as well as taming wild creatures to their will.", "HP: 100", "The Warlock attacks their enemy with a powerful spell.", "The Warlock's tamed creature attacks with all their might often missing but immensely powerful when successful"));
+        list.add(4, new HeroOption(5, "Cleric", "The Cleric is a pacifist who has sworn an oath to cause no harm but help the just.", "HP: 100", "The Cleric heals the hero in most need.", "The Cleric can defy his holy orders and try to resurrect the dead or heal an ally beyond their natural levels."));
+    }
+}
