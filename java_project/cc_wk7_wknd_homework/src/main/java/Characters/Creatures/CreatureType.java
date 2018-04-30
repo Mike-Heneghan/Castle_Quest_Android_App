a674df17544bb3e40e1b53018482c3cadf566721
Mike-Heneghan <m.heneghan@live.co.uk>
2018-04-30 12:15:16 +0100

    Image id added for all chracter types not yet tested.

--- java_project/cc_wk7_wknd_homework/src/main/java/Characters/Creatures/CreatureType.java
+++ java_project/cc_wk7_wknd_homework/src/main/java/Characters/Creatures/CreatureType.java
@@ -4,10 +4,10 @@ import java.util.Random;
 
 public enum CreatureType {
 
-    TROLL("Trolly", 100, 30, 8, 90, 2 ),
-    DRAGON("Dragony", 150, 40, 6, 95, 2),
-    GIANTSPIDER("Spidery", 100, 25, 9, 80, 3),
-    EX("The Dreaded Ex", 150, 20, 6, 60, 12);
+    TROLL("Trolly", 100, 30, 8, 90, 2, 6 ),
+    DRAGON("Dragony", 150, 40, 6, 95, 2, 7),
+    GIANTSPIDER("Spidery", 100, 25, 9, 80, 3, 8),
+    EX("The Dreaded Ex", 150, 20, 6, 60, 12, 9);
 
     public final String name;
     public final int hp;
@@ -15,18 +15,20 @@ public enum CreatureType {
     public final int mv1ChanceValue;
     public final int mv2DamageValue;
     public final int mv2ChanceValue;
+    public final int imageId;
     private static final CreatureType[] VALUES = values();
     private static final int SIZE = VALUES.length;
     private static final Random RANDOM = new Random();
 
 
-    CreatureType(String name, int hp, int mv1DamageValue, int mv1ChanceValue, int mv2DamageValue, int mv2ChanceValue) {
+    CreatureType(String name, int hp, int mv1DamageValue, int mv1ChanceValue, int mv2DamageValue, int mv2ChanceValue, int imageId) {
         this.name = name;
         this.hp = hp;
         this.mv1DamageValue = mv1DamageValue;
         this.mv1ChanceValue = mv1ChanceValue;
         this.mv2DamageValue = mv2DamageValue;
         this.mv2ChanceValue = mv2ChanceValue;
+        this.imageId = imageId;
     }
 
 
@@ -44,6 +46,10 @@ public enum CreatureType {
 
     public int getMv2DamageValue() {
         return mv2DamageValue;
+    }
+
+    public int getImageId() {
+        return imageId;
     }
 
     public String getName() {
