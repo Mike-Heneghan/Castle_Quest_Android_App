e84f3523b87ae23e24b9bab9f3e03190634c057f
Mike-Heneghan <m.heneghan@live.co.uk>
2018-04-30 20:58:51 +0100

    On input of userName the game moves to difficulty selection screen with the userName saved in an Intent.

--- android_project/app/src/main/java/com/example/mike/fantasygame/DragonCastle/Activities/DifficultyActivity.java
+++ android_project/app/src/main/java/com/example/mike/fantasygame/DragonCastle/Activities/DifficultyActivity.java
@@ -2,14 +2,26 @@ package com.example.mike.fantasygame.DragonCastle.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.mike.fantasygame.R;
 
 public class DifficultyActivity extends AppCompatActivity {
 
+    private TextView selectDifficultyTextView;
+    private Button easyDifficultyButton;
+    private Button mediumDifficultyButton;
+    private Button hardDifficultyButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_difficulty);
+
+        selectDifficultyTextView = findViewById(R.id.selectDifficultyTextViewId);
+        easyDifficultyButton = findViewById(R.id.easyDifficultyButtonId);
+        mediumDifficultyButton = findViewById(R.id.mediumDifficultyButtonId);
+        hardDifficultyButton = findViewById(R.id.hardDifficultyButtonId);
     }
 }
--- android_project/app/src/main/java/com/example/mike/fantasygame/DragonCastle/Activities/MainScreen.java
+++ android_project/app/src/main/java/com/example/mike/fantasygame/DragonCastle/Activities/MainScreen.java
@@ -1,17 +1,33 @@
 package com.example.mike.fantasygame.DragonCastle.Activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.mike.fantasygame.R;
 
 public class MainScreen extends AppCompatActivity {
 
+    private EditText userNameEditText;
+    private Button beginAdventureButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
+
+        userNameEditText = findViewById(R.id.userNameEditTextId);
+        beginAdventureButton = findViewById(R.id.beginAdventureButtonId);
     }
 
-
+    public void onBeginAdventureButtonClicked(View button){
+        String userName = userNameEditText.getText().toString();
+        Intent intent = new Intent(this, DifficultyActivity.class);
+        intent.putExtra("userName", userName);
+        startActivity(intent);
+    }
 }
