4a0536be65edfdd429a481e9332e794eb235b254
Mike-Heneghan <m.heneghan@live.co.uk>
2018-04-30 18:22:27 +0100

    Added Main screen and difficulty page activtie layouts.

--- android_project/app/src/main/java/com/example/mike/fantasygame/DragonCastle/Activities/MainScreen.java
+++ android_project/app/src/main/java/com/example/mike/fantasygame/DragonCastle/Activities/MainScreen.java
@@ -12,4 +12,6 @@ public class MainScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
     }
+
+
 }
---
+++ android_project/app/src/main/java/com/example/mike/fantasygame/DragonCastle/DataPeristence/GameState.java
@@ -0,0 +1,4 @@
+package com.example.mike.fantasygame.DragonCastle.DataPeristence;
+
+public class GameState {
+}
