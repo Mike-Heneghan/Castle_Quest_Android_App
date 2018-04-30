14ed62c938cc809aacc78f927df4c1b50a84f416
Mike-Heneghan <m.heneghan@live.co.uk>
2018-04-30 14:02:42 +0100

    All packages moved into DragonCastle

---
+++ java_project/cc_wk7_wknd_homework/src/main/java/DragonCastle/Characters/Heroes/Mages/Mage.java
@@ -0,0 +1,47 @@
+package DragonCastle.Characters.Heroes.Mages;
+
+import DragonCastle.Characters.Creatures.Creature;
+import DragonCastle.Characters.Creatures.CreatureType;
+import DragonCastle.Characters.Character;
+import DragonCastle.Characters.Heroes.Hero;
+
+public abstract class Mage extends Hero {
+
+    private Spell spell;
+    private Creature creature;
+
+    public Mage(String name){
+        super(name, "Attacker");
+        this.spell = Spell.getRandomSpell();
+        this.creature = new Creature(CreatureType.getRandomCreature());
+        //Massive problem still with this random element for newing up objects. Need to fix.
+
+    }
+
+    public Creature getCreature(){
+        return this.creature;
+    }
+
+    public void setCreature(Creature creature) {
+        this.creature = creature;
+    }
+
+    public Spell getSpell() {
+        return spell;
+    }
+
+    public void setSpell(Spell spell) {
+        this.spell = spell;
+    }
+
+    //move 1. This is the move to use the spell that the Mage has assigned to them.
+    public String standardMove(Character characterToAttack){
+        if(shouldDoMove(this.spell.getChanceValue())){
+            characterToAttack.takeDamage(this.spell.getDamageValue());
+            return (this.getName() + " attacked " + characterToAttack.getName() + " successfully.");
+        }
+        return this.getName() + " missed!";
+    }
+
+
+}
---
+++ java_project/cc_wk7_wknd_homework/src/main/java/DragonCastle/Game/Game.java
@@ -0,0 +1,107 @@
+package DragonCastle.Game;
+
+import DragonCastle.Characters.Creatures.Creature;
+import DragonCastle.Characters.Heroes.*;
+import DragonCastle.Characters.Heroes.Fighters.Dwarf;
+import DragonCastle.Characters.Heroes.Fighters.Knight;
+import DragonCastle.Characters.Heroes.Fighters.Barbarian;
+import DragonCastle.Characters.Heroes.Healers.Cleric;
+import DragonCastle.Characters.Heroes.Hero;
+import DragonCastle.Characters.Heroes.Mages.Spell;
+import DragonCastle.Characters.Heroes.Mages.Warlock;
+import DragonCastle.Characters.Narrator;
+import DragonCastle.Room.Room;
+
+import java.util.ArrayList;
+
+public class Game {
+
+
+    private String name;
+    private int roomsToGenerate;
+    private ArrayList<Room> newRooms;
+    private ArrayList<Room> completedRooms;
+    private ArrayList<Hero> heroes;
+
+
+    public Game(String name, int roomsToGenerate){
+
+        this.name = name;
+        this.roomsToGenerate = roomsToGenerate;
+        this.heroes = new ArrayList<>();
+
+        ArrayList<Room> generatedRooms = new ArrayList<>();
+        for (int i = 0; i < roomsToGenerate ; i++) {
+            generatedRooms.add(new Room());
+        }
+        this.newRooms = generatedRooms;
+        this.completedRooms = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<Room> getNewRooms() {
+        return newRooms;
+    }
+
+    public ArrayList<Hero> getHeroes() {
+        return heroes;
+    }
+
+    public void addKnight(String name){
+        heroes.add(new Knight(name));
+    }
+
+    public void addDwarf(String name){
+        heroes.add(new Dwarf(name));
+    }
+
+    public void addBarbarian(String name){
+        heroes.add(new Barbarian(name));
+    }
+
+    public void addCleric(String name){
+        heroes.add(new Cleric(name));
+    }
+
+    public void addWarlock(String name){
+        heroes.add(new Warlock(name));
+    }
+
+    public void addHeroesToFirstRoom(){
+        ArrayList<Hero> heroesInHallway = new ArrayList<>();
+        int heroCount = heroes.size();
+        for (int i = 0; i < heroCount ; i++) {
+            heroesInHallway.add(heroes.remove(0));}
+            newRooms.get(0).addHeroesFromHallway(heroesInHallway);
+        }
+
+    public ArrayList<Room> getCompletedRooms() {
+        return completedRooms;
+    }
+
+    public void returnHeroesFromRoomToHallway(){
+        ArrayList<Hero> heroesInRoom = this.getNewRooms().get(0).getHeroes();
+        ArrayList<Hero> heroesAlive = new ArrayList<>();
+        int heroCount = heroesInRoom.size();
+        for (int i = 0; i < heroCount ; i++) {
+            if(heroesInRoom.get(0).getHp() > 0){
+            heroesAlive.add(heroesInRoom.remove(0));}}
+        for (Hero h: heroesAlive){ heroes.add(h);}
+    }
+
+    public void moveCompletedRoomtoCompletedArrayList(){
+        ArrayList<Room> tempHolding  = new ArrayList<>();
+        tempHolding.add(newRooms.remove(0));
+        completedRooms.add(tempHolding.get(0));
+    }
+
+    public void moveToNextRoom(){
+        returnHeroesFromRoomToHallway();
+        moveCompletedRoomtoCompletedArrayList();
+        addHeroesToFirstRoom();
+    }
+
+}
