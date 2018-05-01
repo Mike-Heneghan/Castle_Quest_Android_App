4c0de9ba80c6bdd5c188f1e6afe4388d5dfaf0f1
Mike-Heneghan <m.heneghan@live.co.uk>
2018-05-01 22:07:30 +0100

    Bug squashed! The Character class is object cpoulnd't be built because it was serilizable.

--- android_project/app/src/main/java/com/example/mike/fantasygame/DragonCastle/Characters/Character.java
+++ android_project/app/src/main/java/com/example/mike/fantasygame/DragonCastle/Characters/Character.java
@@ -3,7 +3,7 @@ package com.example.mike.fantasygame.DragonCastle.Characters;
 import java.io.Serializable;
 import java.util.Random;
 
-public abstract class Character  implements Serializable {
+public class Character  implements Serializable {
 
     protected String name;
     protected int hp;
